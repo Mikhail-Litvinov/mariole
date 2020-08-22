deb4f5d87bbe8c4339c83ef1c0b43893207561a1
Mikhail-Litvinov <69105262+Mikhail-Litvinov@users.noreply.github.com>
2020-08-22 15:31:51 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,62 @@
 // submenu
 let menuBtns = document.getElementsByClassName("menu-btn");
 let main = document.getElementById("main");
+let about = document.getElementById("about");
+let gallery = document.getElementById("gallery");
+let forWomen = document.getElementById("forwomen");
+let forMen = document.getElementById("formen");
+let baby = document.getElementById("baby");
+let blog = document.getElementById("blog");
 let mainSub = document.getElementById("main-sub");
+let aboutSub = document.getElementById("about-sub");
+let gallerySub = document.getElementById("gallery-sub");
+let forWomenSub = document.getElementById("forwomen-sub");
+let forMenSub = document.getElementById("formen-sub");
+let babySub = document.getElementById("baby-sub");
+let blogSub = document.getElementById("blog-sub");
 var i;
 
-function openSubmenu(evt, buttonID) {
-    for (i = 0; i < menuBtns.length; i++) {
-        menuBtns[i].className = menuBtns[i].className.replace(" active", "");
+for (var i=0; i<menuBtns.length; i++) {
+        menuBtns[i].addEventListener('mouseenter', openSubMenu);
+        menuBtns[i].addEventListener('mouseleave', closeSubMenu);
     }
 
-    evt.currentTarget.className += " active"
+function openSubMenu() {
+    this.className += " active";
     if (main.classList.contains("active")) {
-        mainSub.className += " active";
+        mainSub.classList.add("active");
+    }
+    if (about.classList.contains("active")) {
+        aboutSub.className += " active";
+    }
+    if (gallery.classList.contains("active")) {
+        gallerySub.className += " active";
+    }
+    if (forWomen.classList.contains("active")) {
+    forWomenSub.className += " active";
+    }
+    if (forMen.classList.contains("active")) {
+    forMenSub.className += " active";
+    }
+    if (baby.classList.contains("active")) {
+    babySub.className += " active";
+    }
+    if (blog.classList.contains("active")) {
+    blogSub.className += " active";
     }
 }
 
-function closeSub() {
-    for (i = 0; i < menuBtns.length; i++) {
-        menuBtns[i].className = menuBtns[i].className.replace(" active", "");
-    }
-
+function closeSubMenu() {
+    this.classList.remove("active");
     mainSub.classList.remove("active");
+    aboutSub.classList.remove("active");
+    gallerySub.classList.remove("active");
+    forWomenSub.classList.remove("active");
+    forMenSub.classList.remove("active");
+    babySub.classList.remove("active");
+    blogSub.classList.remove("active");
 }
 //submenu end
+
+
+
