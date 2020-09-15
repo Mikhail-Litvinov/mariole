0cd458b15de8651482d6477d7f0169b6f76a444b
OcelotJungle <ocelotjungle@yandex.ru>
2020-09-16 00:50:38 +0500

    Minor changes

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-	$page = array("title" => "TEST TITLE"); // Just for debug
+	$page = array('title' => 'TEST TITLE'); // Just for debug
 	
-	$countries_list = json_decode(file_get_contents("data/countries.json"), true); // JSON file to associative PHP array
+	$countries_list = json_decode(file_get_contents('config/countries.json'), true); // Countries' JSON file to associative PHP array
+	$pages_list = scandir('tpl/pages'); // List of page's templates
 	
-	include_once "tpl/index.tpl"; // Include index.tpl
+	include 'tpl/index.tpl'; // Include index.tpl
