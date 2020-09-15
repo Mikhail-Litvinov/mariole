d2841fed08f9a6cf2e902f6c83897148b5c1caa0
OcelotJungle <ocelotjungle@yandex.ru>
2020-09-16 01:09:27 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 	$page = array('title' => 'TEST TITLE'); // Just for debug
 	
 	$countries_list = json_decode(file_get_contents('config/countries.json'), true); // Countries' JSON file to associative PHP array
-	$pages_list = scandir('tpl/pages'); // List of page's templates
+	$pages_list = array_slice(scandir('tpl/pages'), 2); // List of page's templates
 	
 	include 'tpl/index.tpl'; // Include index.tpl
