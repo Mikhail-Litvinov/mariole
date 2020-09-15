666e07af3a9baeb0e5b42646e57c0b3a0cca5e36
OcelotJungle <ocelotjungle@yandex.ru>
2020-09-16 01:09:25 +0500

    Added comments, minor changes

--- js/navigation.js
+++ js/navigation.js
@@ -1,23 +1,22 @@
-let pages;
+let pages; // Array containing available pages
 
-addEventListener("popstate", () => { showContent() });
+addEventListener("popstate", () => { showContent() }); // Bind forward/back action to showContent
 
-$(document).ready(() => { showContent() });
+$(document).ready(() => { showContent() }); // Execute showContent when page is loaded
 
 function showContent(page) {
-	return;
 	page = validatePage(page ?? location.pathname.slice(1), page ? history.pushState : history.replaceState);
 
-	$("#content").load(`pages/${page}.html`);
+	$("#content").load(`tpl/pages/${page}.tpl`); // Load the page to a content container
 }
 
 function validatePage(page, action) {
-	page = pages.includes(page) ? page : "home";
-	action.bind(history)(null, null, `/${page}`);
+	page = pages.includes(page) ? page : "home"; // If page is provided then use it, else "home"
+	action.bind(history)(null, null, `/${page}`); // If page is given, then make a new history point, else rewrite current history point
 	
-	return page;
+	return page; // Return valid page
 }
 
 function loadPagesList(rawList) {
-	pages = rawList.slice(2).map(item => item.slice(0, -5));
+	pages = rawList.map(item => item.replace(".tpl", "")); // Remove "." and ".." from list, then make a new list without ".tpl" at the end
 }
