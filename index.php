820fa9798a2c3b1e2492052a735f288ec41eb2f7
OcelotJungle <ocelotjungle@yandex.ru>
2020-09-16 16:10:59 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 	
 	$countries_list = json_decode(file_get_contents('config/countries.json'), true); // Countries' JSON file to associative PHP array
 	$pages_list = str_replace('.tpl', '', array_slice(scandir('tpl/pages'), 2)); // List of clean page's template's names
+	$current_page = substr($_SERVER['REQUEST_URI'], 1); // Contains clean current page name
 	
 	include 'tpl/index.tpl'; // Include index.tpl
