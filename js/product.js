2d66f81f1a52fc8577a7988195c591e90bcd9982
OcelotJungle <ocelotjungle@yandex.ru>
2020-10-10 23:19:52 +0500

    Getting data from DB

--- js/product.js
+++ js/product.js
@@ -1,15 +1,10 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+productData = {};
+isProductImagesLoaded = false;
 
-function plusSlide(n) {
-    showSlides(slideIndex += n);
-}
-  
+function setActive(index) { $("div.product-slider-wrapper").attr("active", index); }
+function getActive() { return +($("div.product-slider-wrapper").attr("active")); }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
+// TODO: починить сломанную карусель
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("product-slide");
@@ -26,6 +21,7 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
 }
 
+// TODO: убрать этот срам
 $(window).ready(function () {
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || $(window).width() <= 1024) {
       $("#product-page-wrapper").removeClass("nowrap").addClass("wrap")
@@ -39,3 +35,54 @@ $(window).ready(function () {
       $("#product-page-wrapper").removeClass("wrap").addClass("nowrap")
   }
 })
+
+function loadProductData() {
+	$.getJSON(`/data/database/product_info/${language.get("lang-code")}/${getProductArticle()}`, (data) => {
+		$("title").html(data["name"]);
+		$("#product-data-name").html(data["name"]);
+		$("#product-data-description").html(data["description"]);
+		
+		productData = data;
+		if(!isProductImagesLoaded) {
+			isProductImagesLoaded = true;
+			updateProductImageCarousel();
+		}
+		updateProductPrice();
+	});
+}
+
+function getProductArticle() {
+	return path[1];
+}
+
+function updateProductPrice() {
+	$("#product-data-price").html(`${productData[countries.curname]} ${countries.cursign}`);
+}
+
+function updateProductImageCarousel() {
+	for(let i = 0, img = productData["i0"]; img; img = productData[`i${++i}`]) {
+		let slide = $(`<div class="product-slide fade"><img src="/img/products-images/${img}.jpg" width="100%"></div>`);
+		$(".product-slider").append(slide);
+		
+		let previewSlide = $('<div class="col-1-4 flex-column overflow-hidden"></div>');
+		previewSlide.append($(`<img src="/img/products-previews/${img}.jpg" width="100%" class="demo">`));
+		$(".product-preview > div").append(previewSlide);
+	}
+	if($(".product-slider > .product-slide").length > 1) {
+		$(".product-preview img.demo").each((index, element) => { $(element).click(() => { enableSlide(index); }); });
+	} else {
+		$("a.prev-slide").remove();
+		$("a.next-slide").remove();
+		$("div.product-preview").remove();
+	}
+	moveSlide(0);
+}
+
+$(() => {
+	$("a.prev-slide").click(() => { moveSlide(-1); });
+	$("a.next-slide").click(() => { moveSlide(1); });
+	$(window).on("onlanguagechange", loadProductData);
+	$(window).on("oncountrychange", updateProductPrice);
+	
+	loadProductData();
+});
