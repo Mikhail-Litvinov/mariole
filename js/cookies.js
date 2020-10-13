406710cd903a7900dfadad76286321ad0350de8e
OcelotJungle <ocelotjungle@yandex.ru>
2020-10-13 18:29:23 +0500

    Cookies' lib

---
+++ js/cookies.js
@@ -0,0 +1,40 @@
+cookies = {
+	get: (name) => {
+		let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+		return matches ? decodeURIComponent(matches[1]) : undefined;
+	},
+	set: (name, value, options = {}) => {
+		options = {
+			"path": "/",
+			...options
+		};
+		if(options["expires"] instanceof Date) options["expires"] = options["expires"].toUTCString();
+		let updatedCookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+		for(let optionKey in options) updatedCookie += `; ${optionKey}` + ((options[optionKey] !== true) ? `=${options[optionKey]}` : "");
+		document.cookie = updatedCookie;
+	},
+	remove: (name) => {
+		cookies.set(name, "", { "max-age": -1 });
+	},
+	getItemsInCart: () => {
+		let result = new Map();
+		if(cookies.get("cart")) {
+			for(let item of cookies.get("cart").split("_")) {
+				let record = item.split("-");
+				result.set(+record[0], +record[1]);
+			}
+		}
+		return result;
+	},
+	updateItemsInCart: (newCart) => {
+		let result = [];
+		for(let [article, count] of newCart) if(count) result.push(`${article}-${count}`);
+		cookies.set("cart", result.join("_"), { "max-age": 60*60*24 });
+	},
+	addItemToCart: (article, count) => {
+		cookies.updateItemsInCart(cookies.getItemsInCart().set(article, count));
+	},
+	removeItemFromCart: (article) => {
+		cookies.addItemToCart(article, null);
+	}
+};
