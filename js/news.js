f98430e5343838d4fd3f179516bd81a743ed8b77
OcelotJungle <ocelotjungle@yandex.ru>
2020-10-17 23:20:07 +0500

    Oops, scripts' loading again

--- js/news.js
+++ js/news.js
@@ -2,6 +2,6 @@ function updateNewsSelection() {
 	console.log("Make a view that news were updated");
 }
 
-$(() => {
+$(window).on("onload.news", () => {
 	$(".mobile-nav-button").click(() => { $(".blog-column-right").toggleClass("openned"); });
 });
