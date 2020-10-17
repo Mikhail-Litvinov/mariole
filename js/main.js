849b135d9993fddf012f472cfe88b54429c593e5
OcelotJungle <ocelotjungle@yandex.ru>
2020-10-17 23:24:46 +0500

    More convenient Math functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-Math.bound = (num, min, max) => Math.max(Math.min(num, max), min);
-Math.cycle = (num, border) => Math.abs((num >= 0 ? num : num - Math.floor(num / border) * border) % border);
+Math.bound = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
+Math.cycle = (num, border) => border ? +Math.abs((num >= 0 ? num : num - Math.floor(num / border) * border) % border).toFixed(8) : num;
 
 function updateMobileFlags() {
 	isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
