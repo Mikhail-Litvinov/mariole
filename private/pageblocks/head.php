f2e6672b8735208b50492175873d4f46a725228f
OcelotJungle <ocelotjungle@yandex.ru>
2020-10-23 11:28:20 +0500

    Moved title tag

--- private/pageblocks/head.php
+++ private/pageblocks/head.php
@@ -1,19 +1,19 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/public/css/global/stylesheet.css">
-<link rel="stylesheet" href="/public/css/global/style.css">
-<link rel="stylesheet" href="/public/css/global/media.css">
-<link rel="stylesheet" href="/public/css/global/animate.css">
-<link rel="stylesheet" href="/public/fonts/Fontello/css/animation.css">
-<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello-codes.css">
-<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello-embedded.css">
-<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello-ie7-codes.css">
-<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello-ie7.css">
-<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello.css">
-<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/animation.css">
-<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello-codes.css">
-<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello-embedded.css">
-<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello-ie7-codes.css">
-<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello-ie7.css">
-<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello.css">
-<title></title>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title></title/>
+<link rel="stylesheet" href="/public/css/global/stylesheet.css"/>
+<link rel="stylesheet" href="/public/css/global/style.css"/>
+<link rel="stylesheet" href="/public/css/global/media.css"/>
+<link rel="stylesheet" href="/public/css/global/animate.css"/>
+<link rel="stylesheet" href="/public/fonts/Fontello/css/animation.css"/>
+<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello-codes.css"/>
+<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello-embedded.css"/>
+<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello-ie7-codes.css"/>
+<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello-ie7.css"/>
+<link rel="stylesheet" href="/public/fonts/Fontello/css/fontello.css"/>
+<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/animation.css"/>
+<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello-codes.css"/>
+<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello-embedded.css"/>
+<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello-ie7-codes.css"/>
+<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello-ie7.css"/>
+<link rel="stylesheet" href="/public/fonts/fontello-8780be14/css/fontello.css"/>
