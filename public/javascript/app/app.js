217723d992170a3e84ff6d8c3eec60857a3102e5
OcelotJungle <ocelotjungle@yandex.ru>
2020-10-25 09:16:09 +0500

    Script tags are self-removing

--- public/javascript/app/app.js
+++ public/javascript/app/app.js
@@ -1,3 +1,5 @@
+$("script:not([id])").remove();
+
 Math.bound = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
 Math.cycle = (num, border) => border ? +Math.abs((num >= 0 ? num : num - Math.floor(num / border) * border) % border).toFixed(8) : num;
 
