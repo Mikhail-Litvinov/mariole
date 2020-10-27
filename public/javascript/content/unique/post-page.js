dd24e029a6b521ab1e1b21852f3ab862df79cd11
OcelotJungle <ocelotjungle@yandex.ru>
2020-10-27 08:53:10 +0500

    Simpleslider unloading

--- public/javascript/content/unique/post-page.js
+++ public/javascript/content/unique/post-page.js
@@ -1,3 +1,7 @@
 $(window).on("onload.init_unique/post-page", () => {
 	app.simpleslider = new SimpleSlider();
+	
+	$(window).one("onunload.content", () => {
+		app.simpleslider = undefined;
+	});
 });
