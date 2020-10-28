16a66bc9b3004a4ce84c9f05588c1d94f9abdd95
OcelotJungle <ocelotjungle@yandex.ru>
2020-10-28 22:33:04 +0500

    Login and navigation pages for admin panel

---
+++ admin/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			.login-form-wrapper {
+				width: 20%;
+				height: 10%;
+				margin: 15% 40%;
+				padding: 1em;
+				border: 0.15em solid black;
+			}
+
+			.login-form {
+				margin: 5%;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="login-form-wrapper">
+			<form class="login-form" method="POST">
+				<input type="password" name="password"/>
+				<input type="submit" name="submit" value="Войти"/>
+			</form>
+		</div>
+	</body>
+</html>
