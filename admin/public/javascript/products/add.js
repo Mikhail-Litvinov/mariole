99b0f74263ffb2ac4553097ca55364f691f255b3
OcelotJungle <ocelotjungle@yandex.ru>
2020-10-30 21:55:52 +0500

    More correct crawling

--- admin/public/javascript/products/add.js
+++ admin/public/javascript/products/add.js
@@ -1,4 +1,55 @@
 let languages = ["ru", "en", "cs", "it", "zh"];
+let paramTemplate = undefined;
+let crawler = {
+	crawl(action) {
+		return {
+			action: action,
+			data: crawler.data,
+			prices: crawler.prices,
+			languages: crawler.languages,
+			images: crawler.images,
+			uni_params: crawler.uniParams
+		};
+	},
+	get data() {
+		return ["type", "class"].reduce((result, data) => {
+			result[data] = $(`.js-product-${data}`).val();
+			return result;
+		}, {});
+	},
+	get prices() {
+		return ["rub", "czk", "eur", "usd"].reduce((result, currency) => {
+			result[currency.toUpperCase()] = Math.floor(+$(`.js-price_${currency} input`).val() * 100);
+			return result;
+		}, {});
+	},
+	get languages() {
+		return languages.reduce((result, language) => {
+			result[language] = getProductLanguageData(language);
+			return result;
+		}, {});
+	},
+	get images() {
+		return $.makeArray($(".js-product-images img").map((index, img) => {
+			return img.getAttribute("src").match(/.+\/(.+?)\.jpg/i)[1];
+		}));
+	},
+	get uniParams() {
+		return this.getParams($(".js-uni-params-list"));
+	},
+	getParams(wrapper) {
+		return $.makeArray(wrapper.children(".js-param").map((index, param) => {
+			return {
+				name: param.querySelector(".js-param-name").value,
+				value: param.querySelector(".js-param-value").value
+			};
+		})).filter(param => param.name.length > 0 && param.value.length > 0);
+	}
+};
+
+function getActiveLanguage() {
+	return $(".js-choose-language").val();
+}
 
 function generateProductDatas() {
 	let tpl = $(".js-product-datas > .js-data-template").removeClass("js-data-template").hide();
@@ -15,19 +66,24 @@ function toggleProductData(language = languages[0]) {
 
 function bindEvents() {
 	$(".js-button-save").on("click", () => { save(); });
-	$(".js-choose-language").on("change", (evt) => { toggleProductData(evt.currentTarget.value); });
+	$(".js-choose-language").on("change", () => { toggleProductData(getActiveLanguage()); });
+	$(".js-add-param-btn").on("click", () => { paramTemplate.clone().appendTo(".js-params-list"); });
+	$(".js-add-uni-param-btn").on("click", () => { paramTemplate.clone().appendTo(".js-uni-params-list"); });
+}
+
+function findTemplates() {
+	paramTemplate = $(".js-templates > .js-param-template").removeClass("js-param-template").addClass("js-param").detach();
 }
 
 function save() {
-	console.log(getProductDataAsJSON());
-	// $.post({
-		// url: "/data/edit_product",
-		// data: getProductDataAsJSON(),
-		// dataType: "json",
-		// success: (data) => {
-			// console.log("SAVED!");
-		// }
-	// });
+	// console.log(getProductDataAsJSON("create"));
+	$.post({
+		url: "/data/admin/edit_product",
+		data: { "product_data": getProductDataAsJSON("create") },
+		success: (data) => {
+			console.log(data);
+		}
+	});
 }
 
 function getProductLanguageData(language = languages[1]) {
@@ -35,44 +91,20 @@ function getProductLanguageData(language = languages[1]) {
 	
 	let result = {
 		name: data.find(".js-product-name").val(),
-		description: data.find(".js-product-description").val()
+		description: data.find(".js-product-description").val(),
+		params: crawler.getParams(data.find(".js-params-list"))
 	};
 	
 	return (Object.values(result).every(value => value.length > 0) || language === languages[1]) ? result : getProductLanguageData();
 }
 
-function getProductDataAsJSON() {
-	return JSON.stringify({
-		data: (() => {
-			return ["type", "class"].reduce((result, data) => {
-				result[data] = $(`.js-product-${data}`).val();
-				return result;
-			}, { article: -1 });
-		})(),
-		prices: (() => {
-			return ["rub", "czk", "eur", "usd"].reduce((result, currency) => {
-				result[currency.toUpperCase()] = Math.floor(+$(`.js-price_${currency} input`).val() * 100);
-				return result;
-			}, {});
-		})(),
-		languages: (() => {
-			return languages.reduce((result, language) => {
-				result[language] = getProductLanguageData(language);
-				return result;
-			}, {});
-		})(),
-		images: (() => {
-			return $.makeArray($(".js-product-images img").map((index, img) => {
-				return img.getAttribute("src").match(/.+\/(.+?)\.jpg/i)[1];
-			}));
-		})(),
-		params: {},
-		uni_params: {}
-	});
+function getProductDataAsJSON(action) {
+	return JSON.stringify(crawler.crawl(action));
 }
 
 $(() => {
 	generateProductDatas();
 	toggleProductData();
 	bindEvents();
+	findTemplates();
 });
