93acd8ef991c82e2c1094fc96dc6fed2ecb5d4af
OcelotJungle <ocelotjungle@yandex.ru>
2020-11-04 23:52:43 +0500

    Openable and closeable articles (only ru)

--- public/javascript/content/unique/news.js
+++ public/javascript/content/unique/news.js
@@ -6,6 +6,29 @@ $(window).on("onload.init_unique/news", () => {
 	};
 	
 	$(".mobile-nav-button").click(() => { $(".blog-column-right").toggleClass("openned"); });
+	$(".js-read-more-btn").click((evt) => {
+		let button = $(evt.currentTarget);
+		let content = button.parent().siblings(".news-card-content");
+		let [lastX, lastY] = [window.scrollX, window.scrollY];
+		
+		content.children(".news-card-content-article").height("auto");
+		content.height(content[0].scrollHeight);
+		
+		window.scrollTo(lastX, lastY);
+		
+		button.siblings(".js-hide-btn").show();
+		button.hide();
+	});
+	$(".js-hide-btn").click((evt) => {
+		let button = $(evt.currentTarget);
+		let content = button.parent().siblings(".news-card-content");
+		
+		content.height("");
+		content.children(".news-card-content-article").height("");
+		
+		button.siblings(".js-read-more-btn").show();
+		button.hide();
+	});
 	
 	$(window).on({
 		"onunload.content": () => { app.news = undefined; }
