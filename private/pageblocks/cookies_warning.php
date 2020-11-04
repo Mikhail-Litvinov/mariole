f885866eeea0b5afe4e1356313c77f57825ffebd
OcelotJungle <ocelotjungle@yandex.ru>
2020-11-04 20:11:55 +0500

    Coded cookies warning

---
+++ private/pageblocks/cookies_warning.php
@@ -0,0 +1,14 @@
+<div id="cookies-msg-wrapper" class="cookies-wrapper js-cookies-warning-wrapper" style="display: none">
+    <div class="cookies-content flex wrap flex-align-middle">
+        <div class="cookies-title flex flex-align-middle">
+            <h2 langid="cookies-title"></h2>
+        </div>
+        <div class="cookies-msg">
+            <p langid="cookies-text"></p>
+        </div>
+        <div class="cookies-footer flex nowrap">
+            <a navid="cookies-policy"><button langid="cookies-more"></button></a>
+            <button class="js-accept-cookies-btn" langid="cookies-accept"></button>
+        </div>
+    </div>
+</div>
